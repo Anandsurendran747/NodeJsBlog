e393294ae1495bc264c1faede8236048e7769028
Anand S <anandsurendran747@gmail.com>
2023-11-17 00:43:55 +0530

    new

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({secret:"key",name:'session1',cookie:{maxAge:604800000}}))
+app.use(session({resave:false,saveUninitialized:true,secret:"key",name:'session1',cookie:{maxAge:604800000}}))
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
